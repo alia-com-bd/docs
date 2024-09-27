97f7d42a7b05ebcfa85230b601150c4050579748
Shipan Jodder <158731879+clients-shipanjodder@users.noreply.github.com>
2024-09-28 01:27:41 +0600

    up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,12 +8,12 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'Alia Docs',
+  tagline: 'Explore Documentation by Alia',
+  favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.alia.com.bd',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
